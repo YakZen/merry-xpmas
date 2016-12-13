850bef015af3358e32d047794a47a1d3429d7fc2
Yakzen <steve.hawkes@zeninternet.co.uk>
2016-12-13 18:10:43 +0000

    Solution file fixed and Test project

---
+++ XpMan.XmasTree.Tests/OhSanta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XpMan.XmasTree.Tests
+{
+    [TestClass]
+    public class OhSanta
+    {
+        [TestMethod]
+        public void OmgCanIHazTestz()
+        {
+            bool Testz = false;
+            Assert.IsTrue(Testz);
+        }
+    }
+}
