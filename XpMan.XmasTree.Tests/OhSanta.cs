8ba2a645234638796b92cbaea020d41343bfa134
Yakzen <steve.hawkes@zeninternet.co.uk>
2016-12-14 16:33:20 +0000

    Golden master test

--- XpMan.XmasTree.Tests/OhSanta.cs
+++ XpMan.XmasTree.Tests/OhSanta.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace XpMan.XmasTree.Tests
@@ -7,10 +9,27 @@ namespace XpMan.XmasTree.Tests
     public class OhSanta
     {
         [TestMethod]
-        public void OmgCanIHazTestz()
+        public void KnownGoodOutputTest()
         {
-            bool Testz = false;
-            Assert.IsTrue(Testz);
+            XpMan.XmasTree.Program.Main();
+            StringBuilder goldenMasterBuilder = new StringBuilder();
+            goldenMasterBuilder.AppendLine("A Wooden toy toy has been made for Susan by Green Wooden Toy Making Elf");
+            goldenMasterBuilder.AppendLine("A Wooden toy toy has been made for John by Wooden Toy Making Elf");
+            goldenMasterBuilder.AppendLine("A Train toy has been made for Sarah by Toy Making Elf");
+            goldenMasterBuilder.AppendLine("A Plastic Spaceship toy has been made for Sarah by Plastic Toy Making Elf");
+            goldenMasterBuilder.AppendLine("A Wooden toy toy has been made for Good Tom by Wooden Toy Making Elf");
+            goldenMasterBuilder.AppendLine("Santa's got Susan a Green wooden toy");
+            goldenMasterBuilder.AppendLine("Santa's got Bad Tom a Coal");
+            goldenMasterBuilder.AppendLine("Santa's got John a Wooden toy");
+            goldenMasterBuilder.AppendLine("Santa's got Sarah a Train");
+            goldenMasterBuilder.AppendLine("Santa's got Andy a Plastic Spaceship");
+            goldenMasterBuilder.AppendLine("Santa's got Good Tom a Red wooden toy");
+            var goldenMaster = goldenMasterBuilder.ToString();
+            var actualSantaOutput = Console.PresentsOutput.ToString();
+
+            Assert.AreEqual(goldenMaster,actualSantaOutput);
+            
         }
+        
     }
 }
--- XpMan.XmasTree/Program.cs
+++ XpMan.XmasTree/Program.cs
@@ -1,5 +1,7 @@
 ﻿
 using System;
+using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace XpMan.XmasTree
 {
